a0b5a181f48bdbbcbf9e888ca2f82df7d87a1e6a
Prem Sichanugrist <s@sikac.hu>
2016-01-07 15:33:37 -0500

    Adding (fake?) namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :staff
+  namespace :admin do
+    resources :staff
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
